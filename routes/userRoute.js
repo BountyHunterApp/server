50814f76b7ff9a30ad7c09a8cfffd3af34e705c6
gandryeanb <gstandrianb@gmail.com>
2018-10-11 23:29:46 +0700

    after rebase

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,6 @@ const isLogin = require('../middlewares/isLogin')
 route
   .get('/', isLogin, UserController.getDatauser)
   .put('/', isLogin, UserController.update)
-  .delete('/', isLogin, UserController.delete)
+  .delete('/', isLogin, UserController.delete) 
 
 module.exports = route
