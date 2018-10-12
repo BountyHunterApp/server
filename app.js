a8d06d657f9fea9751ea6b555df4151eec2103fd
gandryeanb <gstandrianb@gmail.com>
2018-10-12 10:11:17 +0700

    done

--- app.js
+++ app.js
@@ -7,22 +7,28 @@ const port = process.env.PORT || 3000
 const db = mongoose.connection
 
 mongoose.set('useCreateIndex', true)
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true })
+mongoose.connect(process.env.DB_HOST, {
+  useNewUrlParser: true
+})
 
 const signUpRoute = require('./routes/signUpRoute')
 const loginRoute = require('./routes/loginRoute')
 const bountyRoute = require('./routes/bountyRoute')
 const uploadRoute = require('./routes/GSCRoute')
+const userRoute = require('./routes/userRoute')
 
 app
   .use(cors())
-  .use(express.urlencoded({extended:false}))
+  .use(express.urlencoded({
+    extended: false
+  }))
   .use(express.json())
 
   .use('/register', signUpRoute)
   .use('/login', loginRoute)
   .use('/bounty', bountyRoute)
   .use('/uploads', uploadRoute)
+  .use('/users', userRoute)
 
   .get('/', (req, res) => {
     res.status(200).json({
@@ -36,6 +42,6 @@ app
 
 db
   .on('error', console.error.bind(console, 'connection error:'))
-  .once('open', function() {
+  .once('open', function () {
     console.log('> DB Connected')
   })
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,39 @@
 const User = require('../models/userModel')
 
 class UserController {
-  
-  static getDatauser (req, res) {
-    res.status(200).json({
-      status: 'success',
-    }) 
+
+  static getDatauser(req, res) {
+    User.findOne({
+        _id: req.decoded.id
+      })
+      .then(data => {
+        res.status(200).json({
+          status: 'success',
+          data: {
+            id: data._id,
+            name: data.name,
+            avatar: data.avatar
+          }
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          status: 'failed',
+          message: err.message
+        })
+      })
   }
 
-  static update (req, res) {
+  static update(req, res) {
     res.status(200).json({
       status: 'success',
-    }) 
+    })
   }
 
-  static delete (req, res) {
+  static delete(req, res) {
     res.status(200).json({
       status: 'success',
-    }) 
+    })
   }
 
 }
